ea4f0757df20902c3167dec8a643e730ec427524
Jack Steffens <jack.steffens@trifork.nl>
2019-06-05 16:11:16 +0200

    FLUENT-1 Work in progress button

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'fluent-button',
-  template: `
-    <p>
-      fluent button works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
 })
 export class FluentButton implements OnInit {
+  @Input() disabled = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
